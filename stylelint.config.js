86ee80c6b74b4e3d43da4ad9f1a4f5b31e08f93b
Junbum Jin <jbjin@isung.com>
2021-08-11 14:49:12 +0900

    chore: add stylelint-config-airbnb package

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   extends: [
     'stylelint-config-standard',
-    'stylelint-config-prettier'
+    'stylelint-config-airbnb',
+    'stylelint-config-prettier',
   ],
+  plugins: ['stylelint-scss', 'stylelint-order'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {}
-}
+  rules: {},
+};
